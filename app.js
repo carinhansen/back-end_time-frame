c5ce6a5748bd8875b8d70ca305a1ad4fb3c4ec69
Carin Hansen <carinhansen95@gmail.com>
2018-04-03 17:47:54 +0200

    Showing package data from package id

--- app.js
+++ app.js
@@ -2,19 +2,39 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const fetch = require("node-fetch");
+const data = require("./MOCK_DATA.json");
 
 const origin = "Rotterdam";
-const destination = "Haarlem";
-const departureTime = "1522774800";
+const destination = "Amsterdam";
 
-app.get('/', (req, res) => {
+app.get('/packages/:packageid', (req, res) => {
+  let departureTime;
+  console.log(req.params);
 
-  fetch("https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=" + origin + "&destinations=" + destination + "mode=driving&departure_time=" + departureTime + "&key=" + process.env.GOOGLE_KEY).then((response) => {
-    return response.json();
-  })
-  .then((response) => {
-    return res.json(response);
-  })
+  for(var i = 0; i < data.length; i++)
+  {
+    if(data[i].packageid == req.params.packageid)
+    {
+      departureTime = data[i].departure_time;
+
+      return fetch("https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=" + origin + "&destinations=" + destination + "mode=driving&departure_time=" + departureTime + "&key=" + process.env.GOOGLE_KEY).then((response) => {
+        return response.json();
+
+      })
+        .then((response) => {
+          let result = {
+            google: response,
+            packageData: data[i]
+          }
+          return res.json(result);
+        });
+
+    } else {
+        return res.status(404).send("Not found");
+    }
+  }
 });
 
 app.listen(8000, () => console.log('Example app listening on port 3000!'))
+
+
