7f5ef025b1082cc5bff0f8e45a012d9f0dc3f0a1
Perry Janssen <perry@burst-digital.com>
2018-06-05 15:49:22 +0200

    split functionality in two endpoints. also, make it work

--- app.js
+++ app.js
@@ -3,48 +3,124 @@ const express = require('express');
 const app = express();
 const fetch = require("node-fetch");
 const data = require("./MOCK_DATA.json");
+const fs = require('fs');
 
-app.get('/packages/:packageid', (req, res) => {
+app.get('/generate/:deliveryid', (req, res) => {
+  
+  data.deliveries.forEach(delivery => {
+
+    if (delivery.id == req.params.deliveryid) {
+
+      let origins = ['3011WN 99'];
+      let destinations = [];
+
+      delivery.packages.forEach(package => {
+
+        const address = package.customer.address.street + " " + package.customer.address.street_number + " " + package.customer.address.postal_code;
+
+        origins.push(address);
+        destinations.push(address);
+
+      });
+
+      origins_string = origins.join('|');
+      destinations_string = destinations.join('|');
+      departureTime = Date.now();
+
+      const url = encodeURI("https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=" + origins_string + "&destinations=" + destinations_string + "&mode=driving&departure_time=" + departureTime + "&key=" + process.env.GOOGLE_KEY);
+
+      return fetch(url).then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+
+        let durations = [];
+
+        for (let i = 0; i < response.rows.length; i++) {
+          durations.push(
+            {
+              package: delivery.packages[i],
+              address: destinations[i],
+              durations: response.rows[i].elements[i],
+              departure_time: delivery.departure_time,
+            }
+          );
+        }
+
+        durations.pop();
+
+        fs.writeFile('delivery_' + delivery.id + '.json', JSON.stringify(durations, null, 2), 'utf8', (err, data) => {
+          if (err) {
+            console.log("er is een error bitch");
+          }
+        });
+
+        return res.send(JSON.parse(fs.readFileSync('delivery_' + delivery.id + '.json', 'utf8')));
+      })
+      .catch((error) => {
+
+        console.log(error);
+
+      });
+
+    }
+
+  });
+
+});
+
+
+app.get('/deliveries/:deliveryid/packages/:packageid', (req, res) => {
   let departureTime;
-  let origin;
-  let destination;
+  let origins;
+  let destinations;
 
   console.log(req.params);
 
-  for(var i = 0; i < data.length; i++)
-  {
-    if(data[i].packageid == req.params.packageid)
-    {
-      departureTime = data[i].departure_time;
-      origin = data[i].origin;
-      destination = data[i].destination;
+  data.deliveries.forEach(delivery => {
 
-      return fetch("https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=" + origin + "&destinations=" + destination + "mode=driving&departure_time=" + departureTime + "&key=" + process.env.GOOGLE_KEY).then((response) => {
-        return response.json();
+    delivery.packages.forEach(package => {
 
-      })
-        .then((response) => {
+      // Package found!
+      if (package.id == req.params.packageid) {
 
-          let durationDelivery = response.rows[0].elements[0].duration_in_traffic.value;
-          let preciseArrival = departureTime + durationDelivery;
+        const json = JSON.parse(fs.readFileSync('delivery_' + req.params.deliveryid + '.json', 'utf8'));
 
-          let minArrival = preciseArrival - 900;
-          let maxArrival = preciseArrival + 900;
+        let index = delivery.packages.indexOf(package);
 
-          let result = {
-            google: response,
-            packageData: data[i],
-            preciseArrival: preciseArrival,
-            minArrival: minArrival,
-            maxArrival: maxArrival,
-          }
-          return res.json(result);
-        });
+        // Add all previous packages durations in traffic + delay time
+        let total_time = 0;
+        for (let i = 0; i < index; i++) {
+          // Voeg de duration_in_traffic van de package toe aan total_time
+          total_time = total_time + json[i].durations.duration_in_traffic.value;
 
-    } else {
-        return res.status(404).send("Not found");
-    }
-  }
+          // Voeg de delay_time van de package toe aan total_time
+          total_time += json[i].package.delivery_delay;
+        }
+
+        let preciseArrival = json[index].departure_time + total_time;
+
+        let minArrival = preciseArrival - 900;
+        let maxArrival = preciseArrival + 900;
+
+        let result = {
+          packageData: package,
+          preciseArrival: preciseArrival,
+          totalTime: total_time,
+          minArrival: minArrival,
+          maxArrival: maxArrival,
+        }
+
+        return res.json(result);
+      } 
+      // else {
+      //     return res.status(404).send("Not found");
+      // }
+
+
+    });
+  });
+
 });
 
 app.listen(8000, () => console.log('Example app listening on port 8000!'))
